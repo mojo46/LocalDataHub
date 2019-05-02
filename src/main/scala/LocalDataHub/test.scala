1ecf0c253b421a006140f7862aa1defc4cf4da91
z024376 <manojmj96@hotmail.com>
2019-05-02 10:01:58 +0530

    local data project

--- src/main/scala/LocalDataHub/test.scala
+++ src/main/scala/LocalDataHub/test.scala
@@ -2,19 +2,18 @@
  * created by z024376
 */
 
-
 package LocalDataHub
 
 import java.io.PrintWriter
-
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{SparkConf, SparkContext, rdd}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.catalyst.parser.CatalystSqlParser
 
 object test {
   val spark = SparkSession.builder().appName("barcode").master("local").getOrCreate()
-
 
   def main(args: Array[String]): Unit = {
     import org.apache.spark.sql
@@ -22,7 +21,10 @@ object test {
 
     val coreDataset = "..\\LocalDataHub\\resources\\core_dataset.csv"
     val schemaFile = "..\\LocalDataHub\\output\\schemaJson.json"
+    val excelFile = ""
 
+
+  //read the file into Dataframe
     val coreDF = spark.read
       .option("inferSchema",value = true)
       .option("header",value = true)
@@ -38,38 +40,105 @@ object test {
     //coreDF.registerTempTable("hivetable")
    // coreDF.schema.prettyJson.foreach(println)
 
-    val data: Seq[(String, DataType, Boolean)] = coreDF.schema.map(m=>(m.name,m.dataType,m.nullable))
-    println("data ====>")
-    data.foreach(println)
-
-    val data1 = Seq("name",1,true)
-    val schema = StructType(Array(StructField("col_name",StringType,true),
-                                  StructField("col_datatype",StringType,true),
-                                  StructField("col_nullabletype",StringType,true)))
-
+    //Create schema for new Table
     import spark.implicits._
-val se = Seq("name","type","nulla")
-    val rdd = spark.sparkContext.parallelize(data)
-    println("print RDD")
-    rdd.foreach(println)
+    val schemas = StructType(Array(StructField("col_name",StringType,true),
+      StructField("col_datatype",StringType,true),
+      StructField("col_nullabletype",BooleanType,true)))
 
-    val dfnew: DataFrame = rdd.toDF()  //["name","type","nulla"]
-    print("dfnew =>>>>")
-    dfnew.show(false)
+    println("schemas ====>")
+    schemas.printTreeString()
+
+    println("\n\n\n")
+/*
+
+    val structfiled=schemas.map(m=>StructField(m.name,m.dataType,m.nullable)).toArray//.foreach(println)
+
+    val structtype:StructType= StructType(structfiled)
+
+    println("\n\n\n")
+    println("Struct type")
+    structtype.printTreeString()
+*/
+    //Create DataFrame form the schema of another table
+    val schemaDf:DataFrame = coreDF.schema.map(m=>(m.name, m.dataType.toString,m.nullable)).toDF("name","type","nulla")
+    println("data ====>")
+    schemaDf.show
+    schemaDf.printSchema()
+    /*sys.exit()*/
+
+    //preparing data for making it into StructField
+    val map1= schemaDf.map(m=>{(m(0).toString, m(1).toString ,m(2).asInstanceOf[Boolean])}).rdd.collect
+    map1.foreach(println)
+
+    val structFiled = map1
+      .map(m=>StructField(m._1, CatalystSqlParser.parseDataType(m._2.replace("Type", "")), m._3))
+
+    //Creating schema from the structfileds --> schema DataFrame
+    val structType1 = StructType(structFiled)
+
+    //creating empty dataframe from Schema - (StructFiled)
+    println("\n\nempty dataframe ====> ")
+    val df = spark.createDataFrame(spark.sparkContext.emptyRDD[Row]/*spark.sparkContext.emptyRDD[Row]*/,structType1)
+
+    df.show()
 
     /*
+        structType1.printTreeString()
+        val carstable = df.registerTempTable("cars")
+        println("\n\nstruct type schema ====> ")
+    */
+
+/*
+    val sampleSchema = StructType(Array(StructField("col1",StringType,true)))
+    sampleSchema.printTreeString()
+*/
+    //val emptydf = spark.emptyDataFrame
+//    sys.exit()
+
+
+    /*
+
+    def Datatype(s:String):DataType{
+
+    val datatype:DataType = s
+
+    }
+    */
+
+/*
+    val data1 = Seq("name",1,true)
+    val schemas = StructType(Array(StructField("col_name",StringType,true),
+                                  StructField("col_datatype",StringType,true),
+                                  StructField("col_nullabletype",BooleanType,true)))
+
+    import spark.implicits._
+    val se = Seq(Row("name","type","nulla"))
+    val rdd = spark.createDataFrame(spark.sparkContext.parallelize(data), schema)
+    println("print RDD")
+//    rdd.foreach(println)
+rdd.show()
+*/
+
+/*
+    val dfnew: DataFrame = rdd.toDF("name","type","nulla")
+    print("dfnew =>>>>")
+    dfnew.show(false)
+*/
+/*
     import spark.implicits._
     val rdd = spark.sparkContext.parallelize(data).map(Row)//.toDF()
-    rdd.show*/
+    rdd.show
     //val nedf = spark.read.json()
 
-/*    val conf = new SparkConf;*/
+    val conf = new SparkConf;
    // val sc = new SparkContext
     //Created Empty RDD
     //var dataRDD = sc.emptyRDD[Row]
 
-    /*val df =  spark.createDataFrame(dataRDD,data)
-df.show()*/
+    val df =  spark.createDataFrame(dataRDD,data)
+df.show()
+*/
 
 /*
     //data.foreach(println)
@@ -117,6 +186,5 @@ df.show()*/
   }
 
 */
-
 
 }
